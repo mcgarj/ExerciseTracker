10e96325004fca82c18ff980f727d6daafa48a0e
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-11-03 08:28:17 +0000

    Initial commit

---
+++ ExerciseTracker/ExerciseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseTrackerApp.swift
+//  ExerciseTracker
+//
+//  Created by McGarry, Joe on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
