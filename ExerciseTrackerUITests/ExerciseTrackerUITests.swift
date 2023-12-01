151441b13c09e482fcf86ccf62e3960d1f297291
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-12-01 10:59:59 +0000

    test: adds UI tests for editing and viewing exercise detail view

--- ExerciseTrackerUITests/ExerciseTrackerUITests.swift
+++ ExerciseTrackerUITests/ExerciseTrackerUITests.swift
@@ -18,7 +18,7 @@ final class ExerciseTrackerUITests: XCTestCase {
         XCTAssertEqual(app.cells.count, 0, "There should be 0 exercises when the app is first launched")
     }
 
-    func testAppAddingExercise() throws {
+    func test_AppAddingExercise() throws {
         app.buttons["Add Exercise"].tap()
 
         let exerciseNameField = app.textFields["exerciseName"]
@@ -30,7 +30,7 @@ final class ExerciseTrackerUITests: XCTestCase {
         XCTAssertEqual(app.cells.count, 1, "There should be 1 exercise after adding test data")
     }
 
-    func testDeletingExercise() throws {
+    func test_DeletingExercise() throws {
         app.buttons["Add Exercise"].tap()
 
         let exerciseNameField = app.textFields["exerciseName"]
@@ -42,6 +42,38 @@ final class ExerciseTrackerUITests: XCTestCase {
         app.staticTexts["Example"].swipeLeft(velocity: XCUIGestureVelocity.slow)
         app.buttons["Delete"].tap()
         XCTAssertEqual(app.cells.count, 0, "There shouldn't be any exercises in the list")
+    }
+
+    func test_EditExercise() throws {
+        app.buttons["Add Exercise"].tap()
+
+        let exerciseNameField = app.textFields["exerciseName"]
+        exerciseNameField.tap()
+        exerciseNameField.typeText("Example")
+
+        app.buttons["doneButton"].tap()
+
+        app.staticTexts["Example"].swipeLeft(velocity: XCUIGestureVelocity.slow)
+        app.buttons["Edit"].tap()
+
+        exerciseNameField.tap()
+        exerciseNameField.typeText(" Changed")
+        app.buttons["doneButton"].tap()
+        XCTAssertEqual(app.cells.count, 1, "There should be 1 exercise after adding test data")
+    }
+
+    func test_ExerciseDetailView() throws {
+        app.buttons["Add Exercise"].tap()
+
+        let exerciseNameField = app.textFields["exerciseName"]
+        exerciseNameField.tap()
+        exerciseNameField.typeText("Example")
+
+        app.buttons["doneButton"].tap()
+        app.staticTexts["Example"].tap()
+
+        XCTAssertEqual(app.cells.count, 4, "There should be 4 pieces of data shown")
+
     }
 
 }
