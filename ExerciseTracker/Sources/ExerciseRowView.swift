acb376bf08c68a887b8a1ecd38cafce46f5c145f
mcgarj <117986969+mcgarj@users.noreply.github.com>
2023-12-06 07:56:57 +0000

    feat: formats weight to 2 decimal places (#5)

--- ExerciseTracker/Sources/ExerciseRowView.swift
+++ ExerciseTracker/Sources/ExerciseRowView.swift
@@ -1,15 +1,7 @@
-//
-//  ExerciseRowView.swift
-//  ExerciseTracker
-//
-//  Created by Joe McGarry on 24/11/2023.
-//
-
 import SwiftUI
 
 struct ExerciseRowView: View {
     @Environment(\.managedObjectContext) private var moc
-
     @ObservedObject var exercise: Exercise
 
     var body: some View {
@@ -19,10 +11,12 @@ struct ExerciseRowView: View {
                     .font(.headline)
                 Text(exercise.category)
             }
+            .padding()
             VStack {
                 Text("Reps: \(exercise.reps)")
-                Text("Weight: \(exercise.weight)")
+                Text("Weight: \(exercise.weight, specifier: "%.2f") kg")
             }
+            .padding()
         }
     }
 }
