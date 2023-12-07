1c44d687da133b4effed42b99c08656054705053
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-12-07 09:35:09 +0000

    chore: add list of features to work on for profile view

--- ExerciseTracker/Sources/ProfileView.swift
+++ ExerciseTracker/Sources/ProfileView.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
+        // MARK: Display dummy user info - name, weight, height, age, gender, exercise goal
+        // MARK: Store user info in CoreData - make new User entity? Use plist instead?
+        // MARK: Once user info has been saved, display in form here
         Text("My profile")
     }
 }
