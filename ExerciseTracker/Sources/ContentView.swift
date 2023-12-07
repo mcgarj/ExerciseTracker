281431cb0924466dceb66cb2ddecb8d0f4aa4bd7
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-12-07 09:12:05 +0000

    feat: adds tab bar view and initial profile view

---
+++ ExerciseTracker/Sources/ContentView.swift
@@ -0,0 +1,81 @@
+import SwiftUI
+
+struct ContentView: View {
+    @FetchRequest(fetchRequest: Exercise.all()) private var exercises
+    @State private var exerciseToEdit: Exercise?
+
+    var provider = DataController.shared
+    var body: some View {
+        NavigationView {
+            ZStack {
+                if exercises.isEmpty {
+                    NoExerciseView()
+                } else {
+                    List {
+                        ForEach(exercises) { exercise in
+                            NavigationLink {
+                                ExerciseDetailView(exercise: exercise)
+                            } label: {
+                                ExerciseRowView(exercise: exercise)
+                                    .swipeActions(allowsFullSwipe: true) {
+                                        Button(role: .destructive) {
+                                            do {
+                                                try delete(exercise)
+                                            } catch {
+                                                print(error)
+                                            }
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
+                                        }
+                                        .tint(.red)
+
+                                        Button {
+                                            exerciseToEdit = exercise
+                                        } label: {
+                                            Label("Edit", systemImage: "pencil")
+                                        }
+                                        .tint(.orange)
+                                    }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Exercise Tracker")
+            .toolbar {
+                Button {
+                    exerciseToEdit = .empty(context: provider.newContext)
+                } label: {
+                    Label("Add Exercise", systemImage: "plus")
+                }
+            }
+            .sheet(item: $exerciseToEdit, onDismiss: {
+                exerciseToEdit = nil
+            }, content: { exercise in
+                NavigationView {
+                    AddExerciseView(viewModel: .init(provider: provider,
+                                                     exercise: exercise))
+                }
+            })
+        }
+    }
+
+}
+
+extension ContentView {
+
+    func delete(_ exercise: Exercise) throws {
+        let context = provider.viewContext
+        let existingExercise =  try context.existingObject(with: exercise.objectID)
+        context.delete(existingExercise)
+        Task(priority: .background) {
+            try await context.perform {
+                try context.save()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExerciseTracker/Sources/ProfileView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("My profile")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
