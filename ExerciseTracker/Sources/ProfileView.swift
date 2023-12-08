6964df4ea9d2ee90c3b75bdfe5b1e3ec56a83bd5
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-12-08 08:19:26 +0000

    feat: Adds static text for profile page layout

--- ExerciseTracker/Sources/ProfileView.swift
+++ ExerciseTracker/Sources/ProfileView.swift
@@ -1,11 +1,24 @@
 import SwiftUI
 
 struct ProfileView: View {
+    let goals = ["Gain muscle mass", "Lose weight", "Tone up"]
+    @State private var selectedGoal = "Tone up"
     var body: some View {
         // MARK: Display dummy user info - name, weight, height, age, gender, exercise goal
         // MARK: Store user info in CoreData - make new User entity? Use plist instead?
         // MARK: Once user info has been saved, display in form here
-        Text("My profile")
+        List {
+            Text("Name: Frodo Baggins")
+            Text("Age: 33")
+            Text("Height: 124 cm")
+            Text("Weight: 45 kg")
+            Text("Gender: Male")
+            Picker("Goal: ", selection: $selectedGoal) {
+                ForEach(goals, id: \.self) {
+                    Text($0)
+                }
+            }
+        }
     }
 }
 
